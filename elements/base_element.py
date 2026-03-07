1ebbe43fe22b80937d75b2846bc0695a96e060e9
hypno-spira <lucy.drow@yandex.ru>
2026-03-07 19:04:17 +0700

    Практическое применение PageFactory в автотестах. Переписываем компонент CourseViewComponent с использованием Page Factory

--- elements/base_element.py
+++ elements/base_element.py
@@ -7,25 +7,21 @@ class BaseElement:
         self.name = name
         self.locator = locator
 
-    # Метод принимает кейворд аргументы (kwargs)
-    def get_locator(self, **kwargs) -> Locator:  # объект Locator для взаимодействия с элементом
-        # Инициализирует объект локатора, подставляя динамические значения в локатор.
+    def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         locator = self.locator.format(**kwargs)
-        # Возвращаем объект локатора
-        return self.page.get_by_test_id(locator)
+        return self.page.get_by_test_id(locator).nth(nth)
 
-    def click(self, **kwargs):
-        # "Лениво" инициализируем локатор
-        locator = self.get_locator(**kwargs)
-        # Выполняем нажатие на элемент
+    def click(self, nth: int = 0, **kwargs):
+        # Добавили аргумент nth и передаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         locator.click()
 
-    def check_visible(self, **kwargs):
-        # Инициализируем локатор "лениво"
-        locator = self.get_locator(**kwargs)
-        # Проверяем, что элемент виден на странице
+    def check_visible(self, nth: int = 0, **kwargs):
+        # Добавили аргумент nth и передаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_be_visible()
 
-    def check_have_text(self, text: str, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def check_have_text(self, text: str, nth: int = 0, **kwargs):
+        # Добавили аргумент nth и передаем его в get_locator
+        locator = self.get_locator(nth, **kwargs)
         expect(locator).to_have_text(text)
